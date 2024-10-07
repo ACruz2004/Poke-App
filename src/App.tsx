0c55a7b7e33cc7065e4aa85ee2ee4d1f7d996d3f
Anthony Cruz <anthonycruz0517@gmail.com>
2024-10-07 16:41:28 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// App 
+
+import ListGroup from "./Components/ListGroup";
+
+function App() {
+  return <div><ListGroup /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// First Full Stack App Using React
+
+function Message() {
+    // This is Javasctript XML
+    const name = 'Antwon';
+    return <h1> Hello  {name} </h1>
+}
+
+export default Message;
