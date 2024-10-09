867681e813e631cef6b2c6c91757b5e1e28918ba
Anthony Cruz <anthonycruz0517@gmail.com>
2024-10-09 18:43:49 -0400

    Paralax Fin

--- src/ImageShrouded.tsx
+++ src/ImageShrouded.tsx
@@ -1,5 +1,5 @@
 import { Parallax } from "react-parallax";
-import Shrouded from "./assets/Shrouded/ShroudedFableBGIMG.png";
+import Shrouded from "./assets/Shrouded/ShroudedFableBGIMG.jpeg";
 
 const ImageShroud = () => (
   <Parallax className="Pimage" bgImage={Shrouded} strength={200}></Parallax>
--- src/ImageStellar.tsx
+++ src/ImageStellar.tsx
@@ -1,5 +1,5 @@
 import { Parallax } from "react-parallax";
-import Stellar from "./assets/Stellar/StellarCrownBGIMG.png";
+import Stellar from "./assets/Stellar/StellarCrownBGIMG.jpeg";
 
 const ImageStellar = () => (
   <Parallax className="Pimage" bgImage={Stellar} strength={200}></Parallax>
