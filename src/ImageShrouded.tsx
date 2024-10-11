27ac22359697a53b465194655e8c9d0840f06a1e
Anthony Cruz <anthonycruz0517@gmail.com>
2024-10-10 20:45:57 -0400

    GOD THIS IS SO COOL

--- src/ImageShrouded.tsx
+++ src/ImageShrouded.tsx
@@ -1,11 +1,23 @@
 import { Parallax } from "react-parallax";
 import Shrouded from "./assets/Shrouded/ShroudedFableBGIMG.jpeg";
 import Logo from "./assets/Shrouded/ShroudedFableLogo.png";
+import card1 from "./assets/Shrouded/ShroudedFableCard1.png";
+import card2 from "./assets/Shrouded/ShroudedFableCard2.png";
+import card3 from "./assets/Shrouded/ShroudedFableOkidogi.png";
 
 const ImageShroud = () => (
   <Parallax className="Pimage" bgImage={Shrouded} strength={200}>
     <div className="Plogo">
       <img src={Logo} alt="" />
+    </div>
+    <div className="Pcard1">
+      <img src={card1} alt="" />
+    </div>
+    <div className="Pcard2">
+      <img src={card2} alt="" />
+    </div>
+    <div className="Pcard3">
+      <img src={card3} alt="" />
     </div>
   </Parallax>
 );
--- src/ImageTwilight.tsx
+++ src/ImageTwilight.tsx
@@ -1,10 +1,27 @@
 import { Parallax } from "react-parallax";
 import Twilight from "./assets/Twilight/TwilightmasBGIMG.jpeg";
 import Logo from "./assets/Twilight/TwilightMasLogo.png";
+import card1 from "./assets/Twilight/TwilightMasCard1.png";
+import card2 from "./assets/Twilight/TwilightMasCard2.png";
+import card4 from "./assets/Twilight/TwilightMasCard3.png";
+import card3 from "./assets/Twilight/TwilightMasEevee.png";
+
 const ImageTwilight = () => (
   <Parallax className="Pimage" bgImage={Twilight} strength={200}>
     <div className="Plogo">
       <img src={Logo} alt="" />
+    </div>
+    <div className="Pcard1">
+      <img src={card1} alt="" />
+    </div>
+    <div className="Pcard2">
+      <img src={card2} alt="" />
+    </div>
+    <div className="Pcard4">
+      <img src={card4} alt="" />
+    </div>
+    <div className="Pcard3">
+      <img src={card3} alt="" />
     </div>
   </Parallax>
 );
