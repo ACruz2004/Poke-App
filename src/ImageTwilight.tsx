146100dda4a8630d087f2e66870d9e702551eb02
Anthony Cruz <anthonycruz0517@gmail.com>
2024-10-28 11:47:21 -0400

    Comments

--- src/ImageTwilight.tsx
+++ src/ImageTwilight.tsx
@@ -1,3 +1,4 @@
+// For Twilght
 import { Parallax } from "react-parallax";
 import Twilight from "./assets/Twilight/TwilightmasBGIMG.jpeg";
 import Logo from "./assets/Twilight/TwilightMasLogo.png";
