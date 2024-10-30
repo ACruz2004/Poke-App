f07d9770c1592745a062ee3639974897e32f98f1
Anthony Cruz <anthonycruz0517@gmail.com>
2024-10-30 17:58:00 -0400

    Sets Page Setup Finished

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import CardItem from "./cardItem";
 import SCImage from "../assets/Cards/SC.jpeg";
+import TMImage from "../assets/Cards/TM.jpg";
+import SFImage from "../assets/Cards/SF.jpeg";
+import TFImage from "../assets/Cards/TF.png";
 
 const Card = () => {
   return (
     <div className="card">
-      <CardItem image={SCImage} />
-      <CardItem />
-      <CardItem />
+      <CardItem
+        image={SCImage}
+        name="Stellar Crown"
+        collected="0/175 (Progress Bar)"
+      />
+      <CardItem
+        image={SFImage}
+        name="Shrouded Fable"
+        collected="0/99 (Progress Bar)"
+      />
+      <CardItem
+        image={TMImage}
+        name="Twilight Masqerade"
+        collected="0/226 (Progress Bar)"
+      />
+      <CardItem
+        image={TFImage}
+        name="Temporal Forces"
+        collected="0/218 (Progress Bar)"
+      />
     </div>
   );
 };
--- src/Components/cardItem.tsx
+++ src/Components/cardItem.tsx
@@ -3,6 +3,8 @@ import "../Style/cards.css";
 
 interface CardItemProps {
   image?: string; // Mark image as optional in case some CardItem doesn't receive an image
+  name?: string;
+  collected?: string;
 }
 
 const CardItem: React.FC<CardItemProps> = (props) => {
@@ -10,8 +12,8 @@ const CardItem: React.FC<CardItemProps> = (props) => {
     <div className="cardBox">
       <img src={props.image} alt="" />
       <div className="setName">
-        <h2></h2>
-        <p className="collectionStats"></p>
+        <h2>{props.name}</h2>
+        <p className="collectionStats"> {props.collected}</p>
         <button className="addTo">Add To My Sets</button>
       </div>
     </div>
