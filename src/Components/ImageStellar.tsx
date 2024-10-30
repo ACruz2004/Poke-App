0ef143a24cf4fe6eb736e486d7734b74af357684
Anthony Cruz <anthonycruz0517@gmail.com>
2024-10-30 15:31:07 -0400

    Stellar Crown Rework

--- src/Components/ImageStellar.tsx
+++ src/Components/ImageStellar.tsx
@@ -2,8 +2,9 @@
 import { Parallax } from "react-parallax";
 import Stellar from "../assets/Stellar/StellarCrownBGIMG.jpeg";
 import Logo from "../assets/Stellar/StellarCrownLogo.png";
-import card1 from "../assets/Stellar/StellarCrownPackB.png";
-import card2 from "../assets/Stellar/StellarCrownPackL.png";
+import card1 from "../assets/Stellar/stellarCrownCard1.png";
+import card2 from "../assets/Stellar/stellarCrownCard2.png";
+import card3 from "../assets/Stellar/stellarCrownCard3.png";
 
 // This is gonna be a pack display not a card one
 const ImageStellar = () => (
@@ -11,11 +12,14 @@ const ImageStellar = () => (
     <div className="Plogo">
       <img src={Logo} alt="" />
     </div>
-    <div className="Ppack1">
+    <div className="Pcard1">
       <img src={card1} alt="" />
     </div>
-    <div className="Ppack2">
+    <div className="Pcard2">
       <img src={card2} alt="" />
+    </div>
+    <div className="Pcard3">
+      <img src={card3} alt="" />
     </div>
   </Parallax>
 );
--- src/View/setsView.tsx
+++ src/View/setsView.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import MakeHead from "../Components/navBar";
 import { View } from "../Components/currentView";
+import ImageTwilight from "../Components/ImageTwilight";
 
 interface ModeProps {
-    toggleNode: number;
-    handleToggle: () => void;
-    handleView: (id: View) => void;
+  toggleNode: number;
+  handleToggle: () => void;
+  handleView: (id: View) => void;
 }
 
-const SetsView: React.FC<ModeProps> = ({ toggleNode, handleToggle, handleView }) => {
-    return (
-        <div>
-        <div className={"container"}>
-            <MakeHead toggleNode={toggleNode} handleToggle={handleToggle} handleView={handleView} />
-        </div>
-        <div>
-            <h1>Quit slacking Anthony1!</h1>
-        </div>
+const SetsView: React.FC<ModeProps> = ({
+  toggleNode,
+  handleToggle,
+  handleView,
+}) => {
+  return (
+    <div>
+      <div className={"container"}>
+        <MakeHead
+          toggleNode={toggleNode}
+          handleToggle={handleToggle}
+          handleView={handleView}
+        />
+      </div>
+      <div>
+        <ImageTwilight />
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default SetsView;
