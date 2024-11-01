716b89847d6f097d9e4e1c4cdc039de68c75e564
DerrekNguyen <mnguye21@kent.edu>
2024-10-31 22:34:32 -0400

    can FINALLY add hashed credentials to database

--- src/View/loginView.tsx
+++ src/View/loginView.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, ChangeEvent } from "react";
 import MakeHead from "../Components/navBar";
 import { View } from "../Components/currentView";
 import MakeBody from "../Components/body";
+import Hash from "../Data/encryptor";
+import axios from "axios";
 
 interface ModeProps {
     toggleNode: number;
@@ -10,16 +12,24 @@ interface ModeProps {
 }
 
 const LoginView: React.FC<ModeProps> = ({ toggleNode, handleToggle, handleView }) => {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:8081/crud')
-            .then(res => res.json())
-            .then(data => setData(data))
-            .catch(err => console.log(err))
-    }, [])
-
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+
+    const handleSubmit = async () => {
+        const hashedName = Hash(username).toString();
+        const hashedPassword = Hash(password).toString();
+
+        try {
+            // { username: hashedName, password: hashedPassword }
+            const response = await axios.post('http://localhost:8080/add-data', { username: hashedName, password: hashedPassword });
+            // alert(response.data);
+            setUsername('');
+            setPassword('');
+            console.log(response);
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    };
 
     return (
         <div>
@@ -29,29 +39,29 @@ const LoginView: React.FC<ModeProps> = ({ toggleNode, handleToggle, handleView }
             <div className="bodyCont">
                 <MakeBody toggleNode={toggleNode} handleToggle={handleToggle} />
             </div>
-            <div className={"inputContainer"}>
-                <form>
-                    <input
-                        value={username}
-                        placeholder="Enter your username here"
-                        onChange={(ev) => setUsername(ev.target.value)}
-                        className={"inputBox"}
-                    />
-                </form>
-            </div>
-            <div className={"inputContainer"}>
-                <form>
-                    <input
-                        value={password}
-                        placeholder="Enter your password here"
-                        onChange={(ev) => setPassword(ev.target.value)}
-                        className={"inputBox"}
-                    />
-                </form>
-            </div>
-            <div className={'inputContainer'}>
-                <button className="inputButton">Log In</button>
-            </div>
+                <div className={"inputContainer"}>
+                    <form>
+                        <input
+                            value={username}
+                            placeholder="Enter your username here"
+                            onChange={(ev) => setUsername(ev.target.value)}
+                            className={"inputBox"}
+                        />
+                    </form>
+                </div>
+                <div className={"inputContainer"}>
+                    <form>
+                        <input
+                            value={password}
+                            placeholder="Enter your password here"
+                            onChange={(ev) => setPassword(ev.target.value)}
+                            className={"inputBox"}
+                        />
+                    </form>
+                </div>
+                <div className={'inputContainer'}>
+                    <button className="inputButton" onClick={handleSubmit}>Log In</button>
+                </div>
         </div>
     );
 }
