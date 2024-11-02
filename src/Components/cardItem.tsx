8225f105cea97c18adb249001a48079a02818100
Anthony Cruz <anthonycruz0517@gmail.com>
2024-11-02 11:49:27 -0400

    Yeuh

--- src/Components/cardItem.tsx
+++ src/Components/cardItem.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import "../Style/cards.css";
 
 interface CardItemProps {
-  image?: string; // Mark image as optional in case some CardItem doesn't receive an image
-  name?: string;
-  collected?: string;
+  image: string;
+  name: string;
+  collected: string;
 }
 
-const CardItem: React.FC<CardItemProps> = (props) => {
+const CardItem: React.FC<CardItemProps> = ({ image, name, collected }) => {
   return (
     <div className="cardBox">
-      <img src={props.image} alt="" />
-      <div className="setName">
-        <h2>{props.name}</h2>
-        <p className="collectionStats"> {props.collected}</p>
-        <button className="addTo">Add To My Sets</button>
+      <div className="imageWrapper">
+        <img src={image} alt={name} />
       </div>
+      <div className="setName">
+        <h2>{name}</h2>
+      </div>
+      <div className="collectionStats">{collected}</div>
+      <button className="addTo">Add to Collection</button>
     </div>
   );
 };
