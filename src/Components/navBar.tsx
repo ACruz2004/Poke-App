dbe1c8691d7cf8cfd1972e09b29e2dc509e3614a
DerrekNguyen <mnguye21@kent.edu>
2024-11-08 16:51:14 -0500

    Logout added.

--- src/Components/navBar.tsx
+++ src/Components/navBar.tsx
@@ -4,6 +4,7 @@ import makeNight from "../assets/day.png";
 import makeDay from "../assets/night.png";
 import React from "react";
 import { View } from "./currentView";
+import { useState, useEffect } from "react";
 
 interface MakeHeadProps {
   toggleNode: number;
@@ -13,6 +14,48 @@ interface MakeHeadProps {
 interface ViewProps {
   handleView: (action: View) => void;
 }
+
+interface LoggingInfoProps {
+  isLogged: string;
+  setIsLogged: () => void;
+  username: string;
+  setUsername: () => void;
+}
+
+const UserIcon = () => {
+  const [isLogged, setIsLogged] = useState(localStorage.getItem("isLogged") === "true");
+  const [username, setUsername] = useState(localStorage.getItem("username"));
+
+  const handleLogout = () => {
+    localStorage.setItem("isLogged", "false");
+    localStorage.removeItem("username");
+
+    setIsLogged(false);
+    setUsername(null);
+
+    window.location.reload()
+  };
+
+  useEffect(() => {
+    const storedIsLogged = localStorage.getItem("isLogged");
+    const storedUsername = localStorage.getItem("username");
+
+    if (storedIsLogged === "true") {
+      setIsLogged(true);
+      setUsername(storedUsername);
+    }
+  }, []);
+
+  return (
+    <>
+      {isLogged && username && (
+        <span className="userIcon" onClick={handleLogout}>
+          {username}
+        </span>
+      )}
+    </>
+  );
+};
 
 const MakeHead: React.FC<MakeHeadProps & ViewProps> = ({
   toggleNode,
@@ -51,6 +94,7 @@ const MakeHead: React.FC<MakeHeadProps & ViewProps> = ({
           Log In
         </li>
       </ul>
+      <UserIcon/>
       <img
         onClick={handleToggle}
         src={toggleNode == 1 ? makeDay : makeNight}
--- src/View/loginView.tsx
+++ src/View/loginView.tsx
@@ -44,6 +44,8 @@ const LogInView: React.FC<DataProps> = ({
         setPassword("");
         localStorage.setItem("username", username);
         localStorage.setItem("isLogged", "true");
+        window.location.reload();
+        console.log(localStorage)
       } catch (error) {
         console.error("There was an error!", error);
         setError(error.response.data);
